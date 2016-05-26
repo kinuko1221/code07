d8d8caa247ce7b211ab2c615870ee6bc47812090
kinuko1221 <kinuko1221@net44-dhcp105.sfc.keio.ac.jp>
2016-05-26 10:31:13 +0900

    練習問題３

--- js/sigma.js
+++ js/sigma.js
@@ -1,1 +1,18 @@
 const N10 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+function squareSum(n){
+    
+}
+function Sum(n){
+     var sum = 0;
+    var i = 1;
+    
+    sum = 0;
+    
+    while(i <= 4){
+        sum = sum + i; //i = 1;
+        i = i + 1;
+    }
+    
+    return sum;
+}
+   
